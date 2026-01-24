7e3dfe3c1e7ef5eda950ee7304373a462b62c5ba
Franz Király <fkiraly@gcos.ai>
2026-01-24 23:40:20 +0100

    Update logger.py

--- src/pytorch_tabular/utils/logger.py
+++ src/pytorch_tabular/utils/logger.py
@@ -1,18 +1,22 @@
 import logging
 import os
-
-from rich.logging import RichHandler
+import sys
 
 
 def get_logger(name):
     logger = logging.getLogger(name)
     # ch = logging.StreamHandler()
     logger.setLevel(level=os.environ.get("PT_LOGLEVEL", "INFO"))
-    formatter = logging.Formatter("%(asctime)s - {%(name)s:%(lineno)d} - %(levelname)s - %(message)s")
-    if not logger.hasHandlers():
-        ch = RichHandler(show_level=False, show_time=False, show_path=False, rich_tracebacks=True)
-        ch.setLevel(logging.DEBUG)
-        ch.setFormatter(formatter)
-        logger.addHandler(ch)
+
+    if not logger.handlers:
+        handler = logging.StreamHandler(sys.stderr)
+        handler.setLevel(logging.DEBUG)
+
+        fmt = "%(asctime)s - {%(name)s:%(lineno)d} - %(levelname)s - %(message)s"
+        formatter = logging.Formatter(fmt)
+        handler.setFormatter(formatter)
+
+        logger.addHandler(handler)
         logger.propagate = False
+
     return logger
